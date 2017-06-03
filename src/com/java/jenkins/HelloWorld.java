1ec2b019d90254105b0aca1d095cff731b83eb42
Rakesh Kumar-XFT <Rakesh Kumar-XFT@MCKINSEY.COM>
2017-06-03 13:19:53 +0530

    added

--- src/com/java/jenkins/HelloWorld.java
+++ src/com/java/jenkins/HelloWorld.java
@@ -4,7 +4,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		System.out.println(": welcome to the jenkins CI");
+		for(int i =0; i<15 ; i++) {
+			System.out.println(" value of i is : ::"+i);
+		}
+		
+		
 	}
+	
+	
 
 }
